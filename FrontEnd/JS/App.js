357e474cc89456ff9ccafabb69b9c8395eacafcb
adonis-26 <ayambndeby@hotmail.fr>
2025-02-18 15:49:25 +0100

    ajout des catégories et des filtres

--- FrontEnd/JS/App.js
+++ FrontEnd/JS/App.js
@@ -1,19 +1,25 @@
 //===============recupération des travaux depuis API============================
 
-async function getWorks() {
-    document.querySelector(".gallery").innerHTML = ""; 
-    const url = "http://localhost:5678/api/works";
-    try {
-      const response = await fetch(url); 
-      if (!response.ok) { 
-        throw new Error(`Reponse status: ${response.status}`);
+async function getWorks(filter) {
+  document.querySelector(".gallery").innerHTML = "";
+  const url = "http://localhost:5678/api/works";
+  try {
+    const response = await fetch(url); 
+    if (!response.ok) { 
+      throw new Error(`Reponse status: ${response.status}`); 
+    }
+    const json = await response.json(); 
+    if (filter) { 
+      const filtrer = json.filter((data) => data.categoryId === filter);
+      for (let i = 0; i < filtrer.length; i++) { 
+        showFigure(filtrer[i]); 
       }
-      const json = await response.json();
-     console.log(json)
-     for(let i = 0; i < json.length; i++) {
-      showFigure(json[i]);
-     }
-     
+    }
+    else {
+      for (let i = 0; i < json.length; i++) { 
+        showFigure(json[i]);
+      }
+    }
     } catch (error) {
       console.error(error.message);
     }
@@ -30,3 +36,32 @@ function showFigure(data) {
   document.querySelector(".gallery").append(figure);
 }
 
+//=============création des boutons categories================
+
+async function createFilterButtons() { 
+  const url = "http://localhost:5678/api/categories"; 
+  try {
+    const response = await fetch(url); 
+    if (!response.ok) { 
+      throw new Error(`Reponse status: ${response.status}`); 
+    }
+    const json = await response.json();
+    for (let i = 0; i < json.length; i++) { 
+      createFilterButton(json[i]); 
+    }
+  } catch (error) {
+    console.error(error.message); 
+  }
+}
+createFilterButtons();
+
+//===========creation des boutons qui permettent de filtrer les projets par catégorie==============
+
+function createFilterButton(data) {
+  const div = document.createElement("div");
+  div.className = data.id;
+  div.addEventListener("click", () => getWorks(data.id)); 
+  div.innerHTML = `${data.name}`;
+  document.querySelector(".div-container").append(div); 
+}
+document.querySelector(".all-categories").addEventListener("click", () => getWorks());

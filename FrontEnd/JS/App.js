8b4784e33ec00857b41b3fe2b63c51167e32d9d4
adonis-26 <ayambndeby@hotmail.fr>
2025-02-18 12:01:34 +0100

    affichage des travaux dans la gallerie

--- FrontEnd/JS/App.js
+++ FrontEnd/JS/App.js
@@ -10,10 +10,23 @@ async function getWorks() {
       }
       const json = await response.json();
      console.log(json)
+     for(let i = 0; i < json.length; i++) {
+      showFigure(json[i]);
+     }
+     
     } catch (error) {
       console.error(error.message);
     }
   }
   getWorks()
-  
-  
+
+//===================afficher les figure==========================
+
+function showFigure(data) { 
+  const figure = document.createElement("figure"); 
+  figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+    <figcaption>${data.title}</figcaption>`;
+
+  document.querySelector(".gallery").append(figure);
+}
+
